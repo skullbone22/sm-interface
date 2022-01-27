daca348177ec2ae80464632cede8aa4578359d4b
skullbone22 <skullbone22@gmail.com>
2022-01-27 08:28:49 +0000

    Added: Requires there to be a directory before launching

--- src/js/pages/Main.js
+++ src/js/pages/Main.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React from "react";
+
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 const Main = () => {
+  var buttonDisabled = { pointerEvents: "none" };
+  var alertShow = "block";
+  var data = JSON.parse(electron.filesApi.getConfig("gameDirectories"));
+  console.log(data);
+  if (data["directory"] !== undefined) {
+    buttonDisabled = { pointerEvents: "all" };
+    alertShow = "none"
+  }
 
-
+  console.log(alertShow)
 
   return (
     <>
       <div>
+        
         <div className="main_title">
           <h1>StowMarines Interface</h1>
         </div>
+        <div style={{width: "50%", marginLeft: "25%", display: alertShow}}>
+          <Alert severity="warning">
+            To continue you must set your Arma 3 directory in settings.
+          </Alert>
+          <br />
+        </div>
         <div className="main_title">
           <div className="playContainer">
-            <a href="#" onClick={() => electron.gameAPI.loadArma()} className="playButton">
+            <a
+              href="#"
+              style={buttonDisabled}
+              onClick={() => electron.gameAPI.loadArma()}
+              className="playButton"
+            >
               START GAME
             </a>
           </div>
           <br />
           <div className="updateContainer">
-            <a href="#" onClick={() => electron.gameAPI.checkForUpdates()} className="updateButton">
+            <a
+              href="#"
+              onClick={() => electron.gameAPI.checkForUpdates()}
+              className="updateButton"
+            >
               Check For Updates
             </a>
           </div>
